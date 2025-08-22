08164252e7ba67b7d5e91c28551df201d7ad546b
Pedro Delmiro <163359112+PedroDelmiro13@users.noreply.github.com>
2025-08-22 22:04:03 +0000

    correção

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,20 @@
-import { Text, View, Image } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image 
-      style={{width:150, height: 150}}
-      source={require('@/assets/images/partial-react-logo.png')}
-      ></Image>
-      <text>Pedro Delmiro</text>
-      <Text>Desenvolvedor backend com foco em java com springboot criando APIs escaláveis.</Text>
-    </View>
-  );
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image 
+      style={{width:150, height: 150}}
+      source={require('@/assets/images/partial-react-logo.png')}
+      />
+      <Text style={{ fontSize: 28, fontWeight: 'bold' }}>Pedro Delmiro</Text>
+      <Text>Desenvolvedor backend com foco em java com springboot criando APIs escaláveis.</Text>
+    </View>
+  );
 }
